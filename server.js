a881c40422f5165e1157e51bf57d135350973815
Kalen <167487804+Ksjames22@users.noreply.github.com>
2024-08-20 19:54:09 -0400

    Basic place holder code

--- server.js
+++ server.js
@@ -18,3 +18,39 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+/**const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const routes = require('./routes/htmlRoutes');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'supersecret',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// Set up Handlebars.js as the template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Use routes
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+ */
