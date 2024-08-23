d8ced2ca9c0cbd883fa76fc528202955c68b459a
Mitchell-610 <Mheit602@gmail.com>
2024-08-22 20:12:02 -0400

    Seeds added in for the pets and the owner whom is posting the adoption.

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,20 @@
 DROP DATABASE IF EXISTS pets_db;
 CREATE DATABASE pets_db;
+
+\c pets_db;
+
+CREATE TABLE owner (
+  id SERIAL PRIMARY KEY,
+  owner_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE pet (
+    id SERIAL PRIMARY KEY,
+    owner_id INT,
+    FOREIGN KEY (movie_id)
+    REFERENCES movies(id)
+    ON DELETE SET NULL
+);
+
+
+-- (name, owner, species, sex, birth, death)('Charlie', 'Alice', 'bird', 'm', '2016-01-18',
